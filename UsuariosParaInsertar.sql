2476f8af940cf59faa52cf65800a50d526163991
Giovanni Roman Fernandez <gio.rom.fdz@gmail.com>
2024-05-09 21:21:04 -0400

    removed unnecessary Insert sql statements

--- UsuariosParaInsertar.sql
+++ UsuariosParaInsertar.sql
@@ -10,9 +10,7 @@ VALUES
   (7,"Holmes Park","1972-04-17","Ap #233-7399 Ornare Rd.","suspendisse@icloud.couk","PSQ67SYD5BD"),
   (8, "Megan Benjamin","1971-03-31","123-6964 Velit Street","mauris.aliquam@protonmail.net","OAI38OKH2GU"),
   (9, "Fletcher Melton","1971-11-24","874-2762 Odio Avenue","elit@hotmail.org","OOX07DOW3KJ"),
-  (10, "Tanisha Peterson","1990-08-09","P.O. Box 396, 1313 Augue Rd.","luctus@icloud.couk","EGU02HXJ1XR");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (10, "Tanisha Peterson","1990-08-09","P.O. Box 396, 1313 Augue Rd.","luctus@icloud.couk","EGU02HXJ1XR"),
   (11, "Cedric Blake","1992-09-22","903-8565 Donec Av.","a.scelerisque@icloud.org","SDV71HNL2KU"),
   (12, "Cody Dixon","1902-01-15","3192 Proin Avenue","nunc.ut@yahoo.net","JGI68JBX0ZR"),
   (13,"Jamal Mcneil","1988-01-18","771-4415 Aliquam Rd.","lacus.aliquam.rutrum@yahoo.ca","VYP57DHO6CO"),
@@ -22,9 +20,7 @@ VALUES
   (17, "Aspen Macias","1971-04-06","1814 Imperdiet Av.","laoreet.lectus.quis@icloud.ca","TXX77RVH9KT"),
   (18, "Malik Hammond","1982-01-26","P.O. Box 650, 9317 Justo. Road","quam@google.couk","TOY42MOS0WJ"),
   (19, "Darrel Dominguez","1997-06-11","P.O. Box 976, 7477 Aliquet. Ave","aliquet.sem@aol.ca","THL75FVO9WW"),
-  (20, "Nadine Blanchard","1998-06-12","412-5526 Consectetuer Road","dui.nec@outlook.ca","CJB91KSU0JH");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (20, "Nadine Blanchard","1998-06-12","412-5526 Consectetuer Road","dui.nec@outlook.ca","CJB91KSU0JH"),
   (21, "Sara Bullock","1972-10-24","P.O. Box 681, 9043 Mauris Road","arcu.ac@hotmail.couk","CBM42VDT5EH"),
   (22, "Yasir Bender","1993-07-04","Ap #291-3034 Magnis Street","felis.ullamcorper.viverra@hotmail.org","LNL32YON7UT"),
   (23, "Calvin O'brien","1990-09-22","697-8860 At Street","diam@protonmail.net","GON74FVW3RB"),
@@ -34,9 +30,7 @@ VALUES
   (27, "Darryl Cruz","1985-05-17","477-3747 Aliquam St.","mauris@hotmail.org","IOH58HIF8SM"),
   (28, "James Watkins","1973-01-21","974-8228 Nunc Street","lacinia.orci@outlook.com","EHZ42CRM9BY"),
   (29, "Abbot Donovan","1973-11-04","P.O. Box 708, 2149 Risus. St.","aliquam.tincidunt.nunc@hotmail.org","BKD56WST3CF"),
-  (30, "Reagan Bauer","1976-09-01","3688 Montes, Road","adipiscing@protonmail.com","LGW34EJK3GG");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (30, "Reagan Bauer","1976-09-01","3688 Montes, Road","adipiscing@protonmail.com","LGW34EJK3GG"),
   (31, "Maya Sexton","1991-05-12","Ap #672-9393 Eu Street","purus.duis@protonmail.ca","DEQ21RUT8DA"),
   (32, "Kevin Riddle","1991-02-26","5345 Non Street","per.inceptos@icloud.ca","AZS47NOR4KG"),
   (33, "Athena Harding","2000-07-26","Ap #302-5942 Velit Avenue","non@yahoo.edu","NXG32CUM4HV"),
@@ -46,9 +40,7 @@ VALUES
   (37, "Hillary Lindsay","1996-04-07","382-6427 Non Av.","erat.semper.rutrum@google.ca","ZKC89GSB1GT"),
   (38, "Cassidy Carver","1998-11-06","P.O. Box 690, 4303 Suspendisse Ave","a@icloud.org","NZO21IDR8KC"),
   (39, "Molly Scott","1994-08-02","530-1847 Et Rd.","cursus.non@outlook.com","YIK67VZY3ZW"),
-  (40, "Isabella Spears","1989-06-27","239-5061 Tellus. Rd.","at@yahoo.couk","LAC47SCU3JL");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (40, "Isabella Spears","1989-06-27","239-5061 Tellus. Rd.","at@yahoo.couk","LAC47SCU3JL"),
   (41, "Jaquelyn Robertson","1974-05-23","Ap #340-9579 Vestibulum. St.","eget.magna.suspendisse@icloud.edu","TAU32PWR8SX"),
   (42, "Duncan Mcconnell","1976-07-01","P.O. Box 248, 127 Nisl Street","purus@protonmail.com","NCG68YWH4KG"),
   (43, "Ignatius Alvarado","2000-10-25","307-7064 Sem Rd.","pretium.aliquet@icloud.ca","MLD38ZJD4YU"),
@@ -58,9 +50,7 @@ VALUES
   (47, "Zeph Rosales","1994-11-08","P.O. Box 973, 4471 Enim. Street","vulputate.lacus.cras@icloud.couk","NOW21ZVN1ZH"),
   (48, "Dieter Green","1985-03-26","8786 Interdum Street","nisl@yahoo.couk","ZWE46RDU4HK"),
   (49, "Dylan Fisher","1972-06-25","Ap #636-258 Neque Street","aliquam.eu@protonmail.net","OPG75OIS4MX"),
-  (50, "Daryl Barrera","1972-01-01","773-9335 Lectus Street","ultrices.a.auctor@hotmail.couk","MGO69WXF1PL");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (50, "Daryl Barrera","1972-01-01","773-9335 Lectus Street","ultrices.a.auctor@hotmail.couk","MGO69WXF1PL"),
   (51, "Kasimir Woods","1980-05-29","238-4258 Vulputate, Av.","nisi.a@icloud.ca","PJC93ZSU8WQ"),
   (52, "Alan O'Neill","2001-05-25","P.O. Box 905, 4529 Eget Avenue","adipiscing.non.luctus@hotmail.org","OCB36OIN7HI"),
   (53, "Caryn Sanchez","2001-07-09","Ap #412-245 Dui, St.","feugiat.lorem.ipsum@yahoo.net","MMH94XFJ2KV"),
@@ -70,9 +60,7 @@ VALUES
   (57, "Dustin Hogan","1974-12-01","752 Dolor, Street","purus.maecenas.libero@aol.com","NKN72PJR3GB"),
   (58, "Denise Holcomb","2000-05-05","P.O. Box 941, 9727 Dolor. Road","sapien.aenean@aol.org","HHX66SLR0KI"),
   (59, "Quentin Cunningham","1992-03-12","357-650 Mauris. Rd.","rhoncus.donec.est@outlook.ca","JCI87EQH3EP"),
-  (60, "Acton Spencer","1997-06-21","Ap #989-1015 Sociis Rd.","a.arcu.sed@aol.edu","BQU13QER2QQ");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (60, "Acton Spencer","1997-06-21","Ap #989-1015 Sociis Rd.","a.arcu.sed@aol.edu","BQU13QER2QQ"),
   (61, "Vivian Andrews","1988-04-27","373-9464 Aliquam St.","mattis.integer.eu@outlook.edu","JJN25JOY0UF"),
   (62, "Carly Tucker","1986-05-22","Ap #296-1572 Fames Ave","sapien.gravida.non@google.edu","KCR18KRY9FO"),
   (63, "Ray Good","2000-08-13","451-4419 Feugiat. Rd.","fringilla@aol.com","HDJ14ODE6HZ"),
@@ -82,9 +70,7 @@ VALUES
   (67, "Serena Frye","1990-05-12","P.O. Box 746, 5971 Ut Rd.","curabitur.consequat@protonmail.net","SDG60CGL3OQ"),
   (68, "Cara Stokes","1989-11-13","147-2629 Magna. Ave","pellentesque.ultricies.dignissim@protonmail.edu","GRC17VZM4QF"),
   (69, "Rylee Weeks","1998-03-01","Ap #598-1498 Malesuada. St.","semper.erat@hotmail.org","NVU18RHN4ET"),
-  (70, "Maxine Hartman","1985-10-24","Ap #300-8422 A Avenue","lorem.sit@protonmail.com","JLS85IPS6SJ");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (70, "Maxine Hartman","1985-10-24","Ap #300-8422 A Avenue","lorem.sit@protonmail.com","JLS85IPS6SJ"),
   (71, "Maxwell Bailey","1985-01-22","3657 Cras Rd.","metus.sit@icloud.com","PCT63HND4MC"),
   (72, "Slade Mcpherson","1996-07-01","Ap #527-9699 Purus, Road","egestas.nunc.sed@google.org","EUS76VZQ3FA"),
   (73, "Megan Cash","1982-08-22","836-443 Felis, Rd.","mollis.phasellus@outlook.org","VWC88OMD4CX"),
@@ -94,9 +80,7 @@ VALUES
   (77, "Rhoda Juarez","1987-01-01","526-8507 Cras Ave","dictum.sapien@aol.edu","BOH43RLC4TQ"),
   (78, "Camilla Duncan","1984-04-28","P.O. Box 300, 8452 Mi Ave","arcu.vestibulum@aol.edu","ICG11OOR2GM"),
   (79, "Winter Glover","1972-08-31","124-7494 Tincidunt Rd.","ipsum.suspendisse@hotmail.ca","RRX71PDP9TC"),
-  (80, "Melanie Hopkins","1994-09-17","P.O. Box 815, 7148 Nunc Rd.","ullamcorper.duis@hotmail.com","NBG77RTP5OT");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (80, "Melanie Hopkins","1994-09-17","P.O. Box 815, 7148 Nunc Rd.","ullamcorper.duis@hotmail.com","NBG77RTP5OT"),
   (81, "Ruby Peterson","1975-02-02","Ap #227-6130 Vitae Street","felis.purus@aol.edu","OWD99HIK6NN"),
   (82, "Murphy Phillips","1979-09-29","7633 Tristique Road","a@protonmail.edu","KMC66AJO2WC"),
   (83, "Otto Bailey","1988-03-31","8661 Dolor St.","orci.luctus@aol.com","VHU63NLX9DB"),
@@ -106,9 +90,7 @@ VALUES
   (87, "Colette Pace","1993-12-22","Ap #400-9897 Consectetuer Street","luctus.felis@icloud.edu","HWK03BEM2KT"),
   (88, "Derek Cooke","2000-08-20","8826 Eu St.","arcu.vestibulum.ut@yahoo.couk","MLX09MPK1FW"),
   (89, "Olivia Franklin","1998-06-15","790-1349 Inceptos Street","lacus.etiam.bibendum@hotmail.couk","BYV42WET7MS"),
-  (90, "Levi Morrow","2001-08-02","Ap #589-9164 Vel St.","a@aol.ca","IOJ49VKG3MU");
-INSERT INTO Usuario (Id, Nombre,FechaNac,Direccion,Email,Password)
-VALUES
+  (90, "Levi Morrow","2001-08-02","Ap #589-9164 Vel St.","a@aol.ca","IOJ49VKG3MU"),
   (91, "Aidan Hopkins","1979-05-06","P.O. Box 357, 5716 Mauris Rd.","metus.urna@aol.com","SGF14WBQ4FK"),
   (92, "Wesley Holder","1972-02-22","522-5898 Diam Avenue","imperdiet.dictum@aol.ca","MGQ33GXB4HS"),
   (93, "Curran Griffin","1996-11-01","689-1887 Cursus. St.","proin.sed@icloud.org","LRX76BCS4UN"),
