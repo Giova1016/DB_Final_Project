3d6be7a1a49c6dbc3ba0ad10113f1e3bfe55379f
Giovanni Roman Fernandez <gio.rom.fdz@gmail.com>
2024-05-13 20:08:11 -0400

    updated the database.php file to have a template only

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
-    $server_name = "localhost:3306"; // or the IP address/hostname of your MySQL server
-    $username = "giovannirf"; // Replace with your username
+    $server_name = "localhost: serverIPaddress"; // or the IP address/hostname of your MySQL server
+    $username = "username"; // Replace with your username
     $password = "password"; //replace with your password
-    // $db_name = "myDB"; // Replace with the name of the database being used
+    $db_name = "myDB"; // Replace with the name of the database being used
 
     // Create Connection
-    $conn = new mysqli($server_name, $username, $password);
+    $conn = mysqli_connect($server_name, $username, $password, $db);
 
     // Check Connection
     if ($conn->connect_error) {
