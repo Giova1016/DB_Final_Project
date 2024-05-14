5def654325997b733250dd5b09d6b3c50a7cc22a
Giovanni Roman Fernandez <gio.rom.fdz@gmail.com>
2024-05-13 20:19:16 -0400

    modified credentials and typo in code for the localhost server.

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
-    $server_name = "localhost: serverIPaddress"; // or the IP address/hostname of your MySQL server
-    $username = "username"; // Replace with your username
-    $password = "password"; //replace with your password
-    $db_name = "myDB"; // Replace with the name of the database being used
+    $server_name = "localhost"; // or the IP address/hostname of your MySQL server
+    $username = "root"; // Replace with your username
+    $password = ""; //replace with your password
+    $db_name = "cocktailbuddydb"; // Replace with the name of the database being used
 
     // Create Connection
-    $conn = mysqli_connect($server_name, $username, $password, $db);
+    $conn = mysqli_connect($server_name, $username, $password, $db_name);
 
     // Check Connection
     if ($conn->connect_error) {
